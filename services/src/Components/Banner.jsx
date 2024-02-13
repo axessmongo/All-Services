1a1fd176bbb421c3b813818d640d4187bc5386db
diwaSelvam <devbharath21k@gmail.com>
2024-02-13 11:23:41 +0530

    create the banner sec

--- services/src/Components/Banner.jsx
+++ services/src/Components/Banner.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import carousel1 from '../assets/images/About2.jpg'
 
 function Banner() {
   return (
@@ -6,13 +7,13 @@ function Banner() {
         <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="..."/>
+      <img src={carousel1} class="d-block w-100" alt="..."/>
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="..."/>
+      <img src={carousel1} class="d-block w-100" alt="..."/>
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="..."/>
+      <img src={carousel1} class="d-block w-100" alt="..."/>
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
