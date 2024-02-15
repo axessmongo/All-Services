e460739decdb13e8ee77d71ebecf3a163b4adc50
Jagan <jagannathan880@gmail.com>
2024-02-15 13:13:49 +0530

    new web page

--- services/src/App.js
+++ services/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import React, { useEffect } from 'react';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Banner from './Components/Banner';
 import About from './Components/About';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 import '../src/assets/style/works.css'
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
